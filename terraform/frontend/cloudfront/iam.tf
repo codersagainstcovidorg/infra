77db9bfec57ba37d7189dc6225547fde341a76c3
test <test>
2020-03-19 23:07:03 -0400

    update policy

--- terraform/frontend/cloudfront/iam.tf
+++ terraform/frontend/cloudfront/iam.tf
@@ -3,7 +3,12 @@ data "aws_iam_policy_document" "github" {
 
   statement {
     actions = [
-      "s3:PutObject"
+      "s3:DeleteObject",
+      "s3:GetBucketLocation",
+      "s3:GetObject",
+      "s3:ListBucket",
+      "s3:PutObject",
+      "s3:PutObjectAcl"
     ]
 
     resources = [
