bd9b55609cf63e8e53d85d4856ebf8deb5921587
Eric Gonzales <guardians611@gmail.com>
2020-03-22 23:36:42 -0400

    Add VPC code

---
+++ terraform/vpc/module/vpc.tf
@@ -0,0 +1,34 @@
+module "label" {
+  source     = "git::https://github.com/cloudposse/terraform-terraform-label.git?ref=0.4.0"
+  namespace  = var.namespace
+  stage      = var.environment
+}
+
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = module.label.id
+  cidr = var.cidr_block
+
+  azs             = var.azs
+  private_subnets = var.private_subnets
+  public_subnets  = var.public_subnets
+
+  enable_nat_gateway = true
+
+  public_dedicated_network_acl = true
+  public_inbound_acl_rules = concat(
+    local.network_acls["default_inbound"],
+    local.network_acls["public_inbound"],
+  )
+  public_outbound_acl_rules = concat(
+    local.network_acls["default_outbound"],
+    local.network_acls["public_outbound"],
+  )
+
+  private_dedicated_network_acl = true
+
+  tags = {
+    Environment = var.environment
+  }
+}
