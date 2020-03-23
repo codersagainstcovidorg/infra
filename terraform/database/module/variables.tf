ac62aa8b3965f941606d43de6f47a5f9591ec809
Eric Gonzales <guardians611@gmail.com>
2020-03-23 11:10:15 -0400

    Add initial database module

---
+++ terraform/database/module/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  description = "aws region"
+  default     = "us-east-1"
+}
+
+variable "environment" {
+  description = "Environment to deploy into"
+}
+
+variable "namespace" {
+  description = "Organization name or abbreviation"
+}
