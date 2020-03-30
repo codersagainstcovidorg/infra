be8dca7a3a976a09c282a7ed40ac0f06cbaebea2
Eric Gonzales <guardians611@gmail.com>
2020-03-30 15:37:29 -0400

    Add data for bastion module

---
+++ terraform/bastion/module/data.tf
@@ -0,0 +1,28 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+data "aws_vpc" "current" {
+  tags = {
+    Environment = var.environment
+  }
+}
+
+data "aws_subnet_ids" "public_subnets" {
+  vpc_id = data.aws_vpc.current.id
+  tags = {
+    Tier = "public"
+  }
+}
