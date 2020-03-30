a9b6678f537172ffa1b7e97508dd8a50e8efa026
Eric Gonzales <guardians611@gmail.com>
2020-03-30 16:15:32 -0400

    remove bastion_ip output

--- terraform/bastion/module/outputs.tf
+++ terraform/bastion/module/outputs.tf
@@ -2,10 +2,6 @@ output "instance_id" {
   value = module.bastion.id[0]
 }
 
-output "bastion_ip" {
-  value = module.bastion.public_ip[0]
-}
-
 output "az" {
   value = module.bastion.availability_zone[0]
 }
