e52e8911de234f1798f7989bcd21526104aabe35
iloveicedgreentea <31193909+iloveicedgreentea@users.noreply.github.com>
2020-04-11 16:47:23 -0400

    add logging

--- terraform/csv_processor/module/cloudwatch.tf
+++ terraform/csv_processor/module/cloudwatch.tf
@@ -1,5 +1,6 @@
 resource "aws_cloudwatch_log_group" "processing" {
-  name = "/lambda/${var.environment}/processing"
+  name = "/aws/lambda/${var.environment}-csv-processor"
+  retention_in_days = 7
 
   tags = {
     Environment = var.environment
